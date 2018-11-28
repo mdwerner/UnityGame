026d3f2047a62e3b5b99a2c097ca73f1f0ad5522
mdwerner <werner.michael.d@gmail.com>
2018-11-28 15:46:38 -0600

    Initial Push
    This setup is a project in which a sprite can move around a top-down
    world with collision enabled.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame, but only after all objects have been processed in Update()
+	void LateUpdate () {
+        transform.position = player.transform.position + offset; //Adjust the camera position to follow the player.
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D player;
+    private SpriteRenderer spriteRenderer;
+    private bool movingLeft;
+    private bool movingRight;
+
+    private void Start(){
+        player = GetComponent<Rigidbody2D> ();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        movingRight = true;
+        movingLeft = false;
+    }//end Start() 
+
+
+    private void FixedUpdate(){
+        Vector3 orig_pos = this.transform.position;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        float newX = orig_pos.x + moveHorizontal * speed;
+        float newY = orig_pos.y + moveVertical * speed;
+
+        player.MovePosition(new Vector2(newX, newY));
+                   
+        if( movingRight && moveHorizontal < 0 ){
+            movingLeft = true;
+            movingRight = false;
+            this.spriteRenderer.flipX = !this.spriteRenderer.flipX;
+            //Debug.Log("Flipping Image From Right to Left.");
+        }else if ( movingLeft && moveHorizontal > 0 ){
+            movingRight = true;
+            movingLeft = false;
+            this.spriteRenderer.flipX = !this.spriteRenderer.flipX;
+            //Debug.Log("Flipping Image From Left to Right.");
+        }//end if-else
+    }//end FixedUpdate()
+
+}//end PlayerController class
